8dc78963c1de9ab2aa63f318a723b968b7c4470b
yihsuanw <yihsuanw@sfu.ca>
2020-11-17 00:22:03 +0800

    created lambda features to create and delete posts
    Yet to be tested. Do not merge with origin yet.

---
+++ lambdaFunctions/create_post.py
@@ -0,0 +1,81 @@
+import json
+# AWS SDK
+import boto3
+from boto3.dynamodb.conditions import Key
+import uuid
+
+# name of the database we are using
+# needs to be edited to YOUR database name
+LEND_DATABASE_NAME = 'ItemsToLend'
+BUY_DATABASE_NAME = 'ItemsToBuy'
+USER_DATABASE_NAME = 'Users'
+
+# create a DynamoDB object using the AWS SDK
+dynamodb = boto3.resource('dynamodb')
+
+def create_post(event, context):
+
+    # get values from api input
+    title = event['title']
+    description = event['description']
+    userId = event['UserId']
+    location_city = event['location_city']
+    location_province = event['location_province']
+    date_posted = event['date_posted']
+    lend = event['lend']
+    buy = event['buy']    
+    # monetary = event['monetary'] 
+    #     # boolean to decide whether for sell or borrow 
+
+    # retrieve information via user profile
+    email = dynamodb.Table(USER_DATABASE_NAME).get_item(
+        Key = {'UserId': userId} 
+        )
+
+    # use the DynamoDB object to select our table
+    if lend:
+        table = dynamodb.Table(LEND_DATABASE_NAME)
+        response = table.put_item(
+            Item = {
+                # 'LendItemId': "L" + str(uuid.uuid4()),
+                'LendItemId': uuid.uuid4(),
+                'title': title,
+                'description': description,
+                'userId': userId,
+                'email': email,
+                'location_city': location_city,
+                'location_province': location_province,
+                'date_posted': date_posted,
+                # 'monetary': monetary,
+                'available': True
+                    # if items are assumed to be always available when first posted,
+                    # available is True by default
+            })
+    elif buy:
+        table = dynamodb.Table(BUY_DATABASE_NAME)
+        price = event['price']
+        response = table.put_item(
+            Item = {
+                # 'BuyItemId': "B" + str(uuid.uuid4()), easier to differentiate b/t buy/ lend w/o having to pass in two booleans
+                'BuyItemId': uuid.uuid4(),
+                'title': title,
+                'description': description,
+                'userId': userId,
+                'email': email,
+                'price': price,
+                'location_city': location_city,
+                'location_province': location_province,
+                'date_posted': date_posted,
+                # 'monetary': monetary,
+                'available': True
+                    # if items are assumed to be always available when first posted,
+                    # available is True by default
+            })
+
+    responseObj = {}
+    responseObj['statusCode'] = 200
+    responseObj['headers'] = {}
+    responseObj['headers']['Content-type'] = 'application/json'
+    responseObj['body'] = json.dumps(response)
+
+    return responseObj
---
+++ lambdaFunctions/delete_post.py
@@ -0,0 +1,36 @@
+from decimal import Decimal
+from pprint import pprint
+import boto3
+from botocore.exceptions import ClientError
+
+# name of the database we are using
+# needs to be edited to YOUR database name
+LEND_DATABASE_NAME = 'ItemsToLend'
+BUY_DATABASE_NAME = 'ItemsToBuy'
+
+# create a DynamoDB object using the AWS SDK
+dynamodb = boto3.resource('dynamodb')
+
+def delete_post(event, context):
+
+    postId = event['postID']
+    lend = event['lend']
+    buy = event['buy']
+    # use the DynamoDB object to select our table
+    if lend:
+        table = dynamodb.Table(LEND_DATABASE_NAME)
+    elif buy:
+        table = dynamodb.Table(BUY_DATABASE_NAME)
+
+    response = table.delete_item(
+        Key={
+            'postID': postId
+        })
+
+    responseObj = {}
+    responseObj['statusCode'] = 200
+    responseObj['headers'] = {}
+    responseObj['headers']['Content-type'] = 'application/json'
+    responseObj['body'] = json.dumps(response)
+
+    return responseObj
