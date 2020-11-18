ed68ee5edeb73b26a08adbf6c6f7dbd45d61ff16
SolomonYu <solomonyu1@gmail.com>
2020-11-17 22:34:29 -0800

    fixed trailing slash on aws, also replaced api url

--- clientPages/constants.js
+++ clientPages/constants.js
@@ -1,2 +1,2 @@
 // change this line to your amazon invoke api url from api gateway.
-const YOUR_API_INVOKE_URL = "https://8ljp3hgfm3.execute-api.us-east-1.amazonaws.com/dev"
+const YOUR_API_INVOKE_URL = "REPLACE ME"
